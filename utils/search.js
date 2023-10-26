1e31f47f035996a2533b3cf229636a2b6a5b5e22
Veizh <maximedurville95300@gmail.com>
2023-10-26 16:38:15 +0200

    fin

---
+++ utils/search.js
@@ -0,0 +1,70 @@
+function actualCount(cards) {
+  let nombre = 0;
+  cards.forEach((card) => {
+    if (card.state === true) {
+      nombre += 1;
+    }
+  });
+
+  document.querySelector(".information-nombre").innerHTML = `${nombre} recette${
+    nombre < 2 ? "" : "s"
+  }`;
+  if(nombre===0){
+    document.querySelector(".noResult").style.display="flex"
+  }
+  else{
+      document.querySelector(".noResult").style.display="none"
+    
+  }
+}
+
+export function search(cards, bool) {
+  let start = +new Date();
+  cards.forEach((e) => {
+    e.filterAll();
+  });
+
+  actualCount(cards);
+  setTimeout(() => {
+    let end = +new Date();
+    let tmp = document.querySelector(".search");
+    console.log(tmp.value, "prends", end - start - 2000 + " ms à etre trié");
+  }, 2000);
+}
+
+
+//
+//verifier ingr et verifier ustensil n'utilise pas la bonne methode
+//
+//
+export function verifierIngr(element, value) {
+  let test = element.map((e) => e.ingredient.toLowerCase());
+  let bl = test.includes(value.toLowerCase());
+
+  return bl;
+}
+export function verifierUstensil(element, value) {
+  let test = element.map((e) => e.toLowerCase());
+  let bl = test.includes(value.toLowerCase());
+
+  return bl;
+}
+export function verifierappareil(element, value) {
+  if (element.toLowerCase() === value.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+export function createArrayFromTags(cards) {
+  let tmp = document.querySelectorAll(".tag");
+  let tags = Array.from(tmp).map((e) => e.textContent);
+  return tags;
+}
+
+export function resetCards(cards) {
+  cards.forEach((e) => {
+    e.toggleState(true);
+  });
+}
